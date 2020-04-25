69060719399d93df957c6981ccdcd219c0486429
harrier999 <harrier999@naver.com>
2020-04-25 23:03:18 +0900

    version2

--- gugudan/src/gugudan/gugudan.java
+++ gugudan/src/gugudan/gugudan.java
@@ -7,12 +7,18 @@ public class gugudan {
 		Scanner scan = new Scanner(System.in);
 		System.out.printf("원하는 단수를 입력하세요 :");
 		a = scan.nextInt();
-		for(int i = 0;i<10;i++) {
-			System.out.printf("%d * %d = %d\n",a,i,a*i);
+		if(a==0) {
+			for(int i = 2;i <10; i++) {
+				for(int j = 1;j<10;j++) {
+					System.out.printf("%d * %d = %d  ",i,j,j*i);
+				}
+				System.out.printf("\n");
+			}
 		}
-		
-		
-		
+		else {
+			for(int i = 1;i<10;i++) {
+				System.out.printf("%d * %d = %d\n",a,i,a*i);
+			}
+		}
 	}
-
 }
